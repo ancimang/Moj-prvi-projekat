337ab48d8ef8d9f2bb6fa92cd5d47627bdfa4053
ancimang <ana.veljovic@outlook.com>
2019-01-10 19:35:05 +0100

    Dodao inicijalnu verziju projekta

---
+++ Termin10Primer04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Termin10Primer04
+{
+    class Program
+    {
+        static void Metoda01()
+        {
+            Console.WriteLine("Da je cutanje snaga a govorenje slabost, vidi se i po tome sto starci i deca vole da pricaju.");
+        }
+
+        static void Metoda02()
+        {
+            // Prazna metoda
+        }
+
+        static void Metoda03()
+        {
+            // Prazna metoda
+        }
+
+        static void Metoda04()
+        {
+            // Prazna metoda
+        }
+
+        static void Main(string[] args)
+        {
+            Metoda01();
+            Metoda02();
+            Metoda03();
+            Metoda04();
+
+            Console.ReadLine();
+        }
+    }
+}
